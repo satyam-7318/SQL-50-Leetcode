c755cb2ef66deb563ee57dea0c8dffa3d1f41f97
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-06 20:32:46 +0530

    Create 04- Article Views.sql

---
+++ 04- Article Views.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/article-views-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+select distinct author_id as id from Views where author_id = viewer_id ORDER BY id;
