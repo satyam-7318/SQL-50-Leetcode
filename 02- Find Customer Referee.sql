745f8d6088ee622a4286b2069ddc321744fbdbd3
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-06 20:37:25 +0530

    Update 02- Find Customer Referee.sql

--- 02- Find Customer Referee.sql
+++ 02- Find Customer Referee.sql
@@ -1,9 +1,3 @@
-/*
+-- https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
 
-https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
-
-*/
-
-
-# Write your MySQL query statement below
 select name from Customer where referee_id IS NULL or referee_id!=2;
