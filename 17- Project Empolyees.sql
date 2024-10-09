bd23e9ba2db1b8e557779ee79185f723a190414d
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-09 22:32:41 +0530

    Create 17- Project Empolyees.sql

---
+++ 17- Project Empolyees.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/project-employees-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+SELECT 
+    p.project_id,
+    ROUND(AVG(e.experience_years), 2) AS average_years
+FROM 
+    Project p
+JOIN 
+    Employee e ON p.employee_id = e.employee_id
+GROUP BY 
+    p.project_id;
