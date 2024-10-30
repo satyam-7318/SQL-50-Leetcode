661aec8a744a19da5efdd60e744e9bea05ef5518
Satyam Tiwari <satyamtiwari2016@gmail.com>
2024-10-30 15:03:39 +0530

    Create 50- List the products.sql

---
+++ 50- List the products.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/list-the-products-ordered-in-a-period/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+select p.product_name, sum(o.unit) as unit from Products p join Orders o on p.product_id=o.product_id where o.order_date>='2020-02-01' and o.order_date<='2020-02-29'
+group by o.product_id having unit>=100 
